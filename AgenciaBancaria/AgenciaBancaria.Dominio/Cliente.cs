251d7fc7409e4e18501bdf862c6437a589c8f677
mauriciopicirillo <mauriciopicirillo@gmail.com>
2021-09-11 17:32:13 -0300

    Initial commit

---
+++ AgenciaBancaria/AgenciaBancaria.Dominio/Cliente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgenciaBancaria.Dominio
+{
+    public class Cliente
+    {
+        public Cliente (string nome, string cpf, string rg, Endereco endereço)
+        {
+            
+            Nome = nome.ValidaStringVazia();
+            CPF = cpf.ValidaStringVazia();
+            RG = rg.ValidaStringVazia();
+            Endereço = endereço ?? throw new Exception("Endereço deve ser informado.");
+        }
+               
+
+        public string Nome { get; private set; }
+        public string CPF { get; private set; }
+        public string RG { get; private set; }
+        public Endereco Endereço { get; private set; }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Agencia-Bancaria
+ Programa basico criado para aprenzigem a orientação a objeto em .Net do bootcamp Decola Tech da Avanade com a professora Carla
